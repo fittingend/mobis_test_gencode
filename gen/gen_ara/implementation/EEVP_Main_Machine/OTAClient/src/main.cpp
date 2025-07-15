f778d82a5fe1174b8f18401d3e144894294379e3
sujin.oh <imsujin@hotmail.com>
2025-07-15 13:41:27 +0900

    test

---
+++ gen/gen_ara/implementation/EEVP_Main_Machine/OTAClient/src/main.cpp
@@ -0,0 +1,12 @@
+#include "ara/core/initialization.h"
+ 
+int main(int argc, char *argv[])
+{
+    ara::core::Initialize();
+    
+    // TODO: your code here
+    
+    ara::core::Deinitialize();
+    
+    return 0;
+}
